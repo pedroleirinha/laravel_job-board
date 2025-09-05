0cb64d4a4c7be54fc8556f3a1e592223c0d79ca6
Pedro Oleirinha <oleirinha.pedro@gmail.com>
2025-09-05 17:59:51 +0100

    added a small internal button for the text input to clear its valu

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,13 @@
-<input
-    class="w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 focus:ring-2"
-    type="text" id="{{ $name }}" name="{{ $name }}" value="{{ $value }}" placeholder="{{ $placeholder }}" />
+<div class="relative">
+    <input
+        class="w-full pr-8 rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 focus:ring-2"
+        type="text" id="{{ $name }}" name="{{ $name }}" value="{{ $value }}" placeholder="{{ $placeholder }}" />
+
+    <button class="absolute top-0 right-0 flex h-full items-center pr-2" 
+        onclick="document.getElementById('{{ $name }}').value = ''">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="h-4 w-4 text-slate-500">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+        </svg>
+    </button>
+</div>
