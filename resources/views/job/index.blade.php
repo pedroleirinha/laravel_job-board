1ce67e52fae92796fb48558553c77581ebee01d4
Pedro Oleirinha <oleirinha.pedro@gmail.com>
2025-09-09 14:24:32 +0100

    added alpine js to handle the clear form text fields and submit the form through code

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,18 +1,21 @@
 <x-layout>
     <x-bread-crumbs class="mb-4" :links="['Jobs' => route('jobs.index')]" />
 
-    <x-card class="mb-4 text-sm">
-        <form action="{{ route('jobs.index') }}" method="GET">
+    <x-card class="mb-4 text-sm" x-data="">
+        <form action="{{ route('jobs.index') }}" x-ref="filters" method="GET" id="filtering-form">
             <div class="mb-4 grid grid-cols-2 gap-4">
                 <div>
                     <div class="mb-1 font-semibold">Search</div>
-                    <x-text-input name="search" :value="request('search')" placeholder="Search for any text" />
+                    <x-text-input formRef="filters" name="search" :value="request('search')"
+                        placeholder="Search for any text" />
                 </div>
                 <div>
                     <div class="mb-1 font-semibold">Salary</div>
                     <div class="flex space-x-2">
-                        <x-text-input name="min_salary" :value="request('min_salary')" placeholder="From" />
-                        <x-text-input name="max_salary" :value="request('max_salary')" placeholder="To" />
+                        <x-text-input formRef="filters" name="min_salary" :value="request('min_salary')"
+                            placeholder="From" />
+                        <x-text-input formRef="filters" name="max_salary" :value="request('max_salary')"
+                            placeholder="To" />
                     </div>
                 </div>
                 <div>
@@ -24,7 +27,7 @@
                     <x-radio-group name="category" :options="\App\Models\Job::$categories" />
                 </div>
             </div>
-            <button class="w-full" type="submit">Filter</button>
+            <x-button class="w-full">Filter</x-button>
         </form>
     </x-card>
     @foreach ($jobs as $job)
